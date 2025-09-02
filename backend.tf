f0d6dbb28d123e040ad20fa674294d11c5ede562
shimul <h.shimul921@gmail.com>
2025-09-02 13:10:53 -0400

    us-east-2

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "atlantis-tf-state-shimul"  # <- change
     key            = "iam-users/terraform.tfstate"
-    region         = "us-east-1"                     # <- change if needed
+    region         = "us-east-2"                     # <- change if needed
     dynamodb_table = "atlantis-tf-locks"
     encrypt        = true
   }
