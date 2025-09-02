9cbf6a40b8e2d44281031dff4f8bec39092a2d70
shimul <h.shimul921@gmail.com>
2025-09-02 15:31:58 -0400

    updated code

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
 output "created_usernames" {
-  value       = keys(aws_iam_user.this)
-  description = "All usernames created by this stack."
-}
-
-output "access_key_ids" {
-  value       = { for k, v in aws_iam_access_key.this : k => v.id }
-  description = "Access key IDs (no secrets). Secrets are stored in state and optionally SSM."
-  sensitive   = true
+  description = "All IAM usernames created."
+  value       = [for _, u in aws_iam_user.this : u.name]
 }
