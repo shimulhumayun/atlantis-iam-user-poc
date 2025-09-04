6d17fe34db4d2014c12074683722eccbc43d4911
Humayun Shimul <h.shimul921@gmail.com>
2025-09-04 12:43:57 -0400

    Add Terraform configuration for request REQ-20250904-B20D0E34

---
+++ terraform/REQ-20250904-B20D0E34.tf
@@ -0,0 +1,10 @@
+
+resource "null_resource" "access_request_REQ-20250904-B20D0E34" {
+  triggers = {
+    request_id = "REQ-20250904-B20D0E34"
+    user_email = "jane.smith@company.com"
+    resource_type = "snowflake_new_account"
+    resource_id = "h.shimul921@gmail.com"
+    justification = "this a nother thththth"
+  }
+}
