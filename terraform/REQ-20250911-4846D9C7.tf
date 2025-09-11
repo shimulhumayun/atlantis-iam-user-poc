7b7853de783078c992f3832b7b3fa55e07c66c03
Humayun Shimul <h.shimul921@gmail.com>
2025-09-11 16:12:19 -0400

    Add Terraform configuration for request REQ-20250911-4846D9C7

---
+++ terraform/REQ-20250911-4846D9C7.tf
@@ -0,0 +1,10 @@
+
+resource "null_resource" "access_request_REQ-20250911-4846D9C7" {
+  triggers = {
+    request_id = "REQ-20250911-4846D9C7"
+    user_email = "jane.smith@company.com"
+    resource_type = "database"
+    resource_id = "db"
+    justification = "sdsdsdsdfffffffffffffffffffffffff"
+  }
+}
